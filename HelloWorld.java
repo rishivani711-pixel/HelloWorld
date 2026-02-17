5d325ddbece0ef71633fccf50ffb746b6bf78043
rishivani711-pixel <rishivani711@gmail.com>
2026-02-17 15:20:01 +0530

    UC2: Implement OOPS banner using ASCII art

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
 
 public class HelloWorld{
  public static void main(String args[]){
-    System.out.println("Hello World");
+    System.out.println(" *****  " + " *****  " + " *****  " + " ***** ");
+System.out.println("**   ** " + "**   ** " + "**   ** " + "**     ");
+System.out.println("**   ** " + "**   ** " + "**   ** " + "**     ");
+System.out.println("**   ** " + "**   ** " + " *****  " + " ***** ");
+System.out.println("**   ** " + "**   ** " + "**      " + "     **");
+System.out.println("**   ** " + "**   ** " + "**      " + "     **");
+System.out.println(" *****  " + " *****  " + "**      " + " ***** ");
     }
 }
 
